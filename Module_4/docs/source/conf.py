91d92444ea37c263fc323608055f5fe951adb5eb
Jennifer Feng <jennifer@Jennifers-iMac.local>
2026-06-14 22:18:01 -0500

    add sphinx

--- Module_4/docs/source/conf.py
+++ Module_4/docs/source/conf.py
@@ -17,8 +17,8 @@ release = '1.0'
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(".."))
-
+sys.path.insert(0, os.path.abspath("../../src"))
+sys.path.insert(0, os.path.abspath("../../src/Module_2"))
 
 extensions = ['sphinx.ext.autodoc']
 
